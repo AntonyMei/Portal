12601317d7c9e1a07969466d13a874c94bc2b96d
Antony <meiyixuan2000@126.com>
2020-02-07 02:06:27 +0800

    Add anchor

---
+++ Assets/Scripts/Player/ConnectionController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionController : MonoBehaviour
+{
+    [Header("Anchor Settings")]
+    [Tooltip("The material when anchor is not active")]
+    public Material NormalAnchorMaterial;
+    [Tooltip("The material when anchor is active")]
+    public Material ActiveAnchorMaterial;
+
+    [Header("Ray Settings")]
+    [Tooltip("The material attached to rays")]
+    public Material RayMaterial;
+    [Tooltip("The mesh of the ray")]
+    public Mesh RayMesh;
+    [Tooltip("The radius of the ray")]
+    public float Radius = 0.1f;
+
+    [Header("Cursor settings"), Range(0f, 100f)]
+    [Tooltip("The sensitivity of aided aiming")]
+    public float AidedAimingSensitivity = 20f;
+
+    [Header("Dependencies")]
+    [Tooltip("The ray controller attached to the player")]
+    public RayController RayController;
+    [Tooltip("The camera attached to the player")]
+    public Camera PlayerCamera;
+
+    private bool is_first_anchor_set = false;
+    private GameObject first_anchor = null;
+
+    private Stack<GameObject> ray_list = new Stack<GameObject>();
+    private GameObject ray_root;
+
+    void Start() {
+        ray_root = new GameObject("RayRoot");
+    }
+    void Update() {
+        // Cast a virtual ray to detect
+        RaycastHit hit_info;
+        Ray ray = new Ray(PlayerCamera.transform.position,
+                          PlayerCamera.transform.forward);
+        Physics.Raycast(ray, out hit_info);
+        // React to clicks
+        if (Input.GetKeyDown(KeyCode.Mouse1)) {
+            if (!is_first_anchor_set) {
+                if (hit_info.distance != 0 && hit_info.transform.tag == "Anchor") {
+                    MeshRenderer renderer = hit_info.transform.GetComponent<MeshRenderer>();
+                    renderer.material = ActiveAnchorMaterial;
+                    is_first_anchor_set = true;
+                    first_anchor = hit_info.transform.gameObject;
+                }
+            } else {
+                if (hit_info.distance != 0 && hit_info.transform.tag == "Anchor") {
+                    // Change color
+                    MeshRenderer renderer = hit_info.transform.GetComponent<MeshRenderer>();
+                    renderer.material = ActiveAnchorMaterial;
+                    // Generate ray
+                    GameObject ray_obj = new GameObject();
+                    ray_obj.name = $"{first_anchor.name}to{hit_info.transform.name}";
+                    ray_obj.transform.parent = ray_root.transform;
+                    // Render ray
+                    RayRenderer ray_renderer = ray_obj.AddComponent<RayRenderer>();
+                    ray_renderer.Radius = Radius;
+                    ray_renderer.SetRenderer(RayMaterial, RayMesh);
+                    ray_renderer.RenderWithGameObjects(first_anchor, hit_info.transform.gameObject);
+                    ray_renderer.StartRendering();
+                    ray_list.Push(ray_obj);
+                    is_first_anchor_set = false;
+                    first_anchor = null;
+                } else {
+                    MeshRenderer renderer = first_anchor.GetComponent<MeshRenderer>();
+                    renderer.material = NormalAnchorMaterial;
+                    is_first_anchor_set = false;
+                    first_anchor = null;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Scene/RayRenderer.cs
+++ Assets/Scripts/Scene/RayRenderer.cs
@@ -1,26 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(MeshRenderer))]
+
 public class RayRenderer : MonoBehaviour {
     [Header("Render Settings")]
     [Tooltip("The radius of the ray")]
     public float Radius = 0.001f;
+    [Tooltip("If active, the end point will be automatically refreshed")]
+    public bool UseGameObjectAsEndpoint = false;
+    [Tooltip("The start of the ray, only works when UseGameObjectAsEndpoint is active")]
+    public GameObject Start = null;
+    [Tooltip("The end of the ray, only works when UseGameObjectAsEndpoint is active")]
+    public GameObject End = null;
 
     [HideInInspector]
     public Vector3 StartPoint = new Vector3();
     [HideInInspector]
     public Vector3 EndPoint = new Vector3();
+    [HideInInspector]
+    public MeshRenderer MeshRenderer = null;
+    [HideInInspector]
+    public MeshFilter MeshFilter = null;
     
-    void OnEnable() { StopRendering(); }
+    void OnEnable() { 
+        if (!TryGetComponent<MeshRenderer>(out MeshRenderer)) {
+            MeshRenderer = gameObject.AddComponent<MeshRenderer>();
+        }
+        if (!TryGetComponent<MeshFilter>(out MeshFilter)) {
+            MeshFilter = gameObject.AddComponent<MeshFilter>();
+        }
+        if (isRayRendering()) StopRendering();
+    }
+    void Update() {
+        if (UseGameObjectAsEndpoint) {
+            Refresh(Start.transform.position, End.transform.position);
+        }
+    }
     public void StartRendering() { 
-        GetComponent<MeshRenderer>().enabled = true;
+       MeshRenderer.enabled = true;
     }
     public void StopRendering() {
-        GetComponent<MeshRenderer>().enabled = false;
+        MeshRenderer.enabled = false;
     }
     public bool isRayRendering() {
-        return GetComponent<MeshRenderer>().enabled;
+        return MeshRenderer.enabled;
+    }
+    public void SetRenderer(Material material, Mesh mesh) {
+        MeshRenderer.material = material;
+        MeshFilter.mesh = mesh;
     }
     /// <summary>
     /// Should be called after the start and end is set
@@ -50,5 +77,9 @@ public class RayRenderer : MonoBehaviour {
         Vector3 axis = Vector3.Cross(dir, new Vector3(0, 1, 0));
         transform.eulerAngles = new Vector3(0, 0, 0);
         transform.Rotate(axis, -angle);
+    }
+    public void RenderWithGameObjects(GameObject start, GameObject end) {
+        UseGameObjectAsEndpoint = true;
+        Start = start; End = end;
     }
 }
