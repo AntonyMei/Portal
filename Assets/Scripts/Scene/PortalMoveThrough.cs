4c0875bde47aca780e6d37c0ef390a9f266c1359
Antony <meiyixuan2000@126.com>
2020-05-02 01:05:16 +0800

    Modified Scene

---
+++ Assets/Scripts/Scene/PortalMoveThrough.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMoveThrough : MonoBehaviour {
+    public GameObject Player;
+    public GameObject Destination;
+    public PortalActivator PortalActivator;
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            Player.transform.position = Destination.transform.position;
+            // Disable the portal camera to reduce cost
+            foreach(var obj in PortalActivator.ActivationList) {
+                obj.SetActive(false);
+            }
+        }
+    }
+}
