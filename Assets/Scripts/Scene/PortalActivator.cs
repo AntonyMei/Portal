f36f78c0b9b2fdb729ba21b9b45c45bf4e9fba65
Antony <meiyixuan2000@126.com>
2020-05-01 17:59:05 +0800

    Modified Scene

---
+++ Assets/Scripts/Scene/PortalActivator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalActivator : MonoBehaviour {
+    /* Objects to activate */
+    public GameObject[] ActivationList;
+    public double ActivateThreshold;
+    public GameObject LeftColumn;
+    public GameObject RightColumn;
+
+    /* Materials */
+    public Material ActivePortalMaterial;
+    public Material HalfActivePortalMaterial;
+    public Material ColumnGlowMaterial;
+
+    public void ActivatePortal(double input_strength) {
+        if(input_strength < ActivateThreshold) {
+            GetComponent<MeshRenderer>().material = HalfActivePortalMaterial;
+        } else {
+            GetComponent<MeshRenderer>().material = ActivePortalMaterial;
+            foreach(GameObject obj in ActivationList) {
+                obj.SetActive(true);
+            }
+            LeftColumn.GetComponent<MeshRenderer>().materials[1] = ColumnGlowMaterial;
+            RightColumn.GetComponent<MeshRenderer>().materials[1] = ColumnGlowMaterial;
+        }
+    }
+}
