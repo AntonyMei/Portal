1a8db847c1e026cfcad3ec0471f6606dc2b8d267
Antony <meiyixuan2000@126.com>
2020-05-02 22:13:43 +0800

    Modified Scene

---
+++ Assets/Scripts/Scene/EndScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScene : MonoBehaviour {
+    public Image BlackBackGround;
+    public Text EndSceneText;
+    void OnTriggerEnter(Collider other) {
+        if (other.transform.tag == "Player") {
+            BlackBackGround.enabled = true;
+            EndSceneText.enabled = true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+/Bin/Portal.exe 为可执行文件，目前仅有教学关卡的Demo。运行需要.net framework, 请确保安装。
+由于版权问题，BGM暂时无法加入到游戏本体中，后续（暑期？咕咕咕）在steam平台发布时将会包含BGM。
